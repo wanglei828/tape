299302b89d33a8be800631a3d3994802ade04af0
Yi Wang <yi.wang.2005@gmail.com>
2018-06-20 15:20:21 -0700

    Add paddle/fluid/string

--- tape/variable.cc
+++ tape/variable.cc
@@ -12,9 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include "src/variable.h"
-
-#include "src/tape.h"
+#include "tape/variable.h"
 
 namespace paddle {
 namespace tape {
