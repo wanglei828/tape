2fcf7f0617cc69c2faed4749ccba4ea9b94b5045
Yi Wang <yi.wang.2005@gmail.com>
2018-06-20 22:04:12 -0700

    Make place no longer require boost

---
+++ paddle/fluid/platform/place.cc
@@ -0,0 +1,92 @@
+/* Copyright (c) 2016 PaddlePaddle Authors. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License. */
+
+#include "paddle/fluid/platform/place.h"
+
+namespace paddle {
+namespace platform {
+
+static Place* the_current_place = new CPUPlace();
+
+Place* clone_place(const Place& p) {
+  Place* r = nullptr;
+  if (is_cpu_place(p))
+    r = new CPUPlace();
+  else if (is_gpu_place(p))
+    r = new CUDAPlace(dynamic_cast<const CUDAPlace&>(p).device);
+  else if (is_cuda_pinned_place(p))
+    r = new CUDAPinnedPlace();
+  return r;
+}
+
+void set_place(const Place &p) {
+  delete the_current_place;
+  the_current_place = clone_place(p);
+}
+
+const Place &get_place() { return *the_current_place; }
+
+const CUDAPlace default_gpu() { return CUDAPlace(0); }
+const CPUPlace default_cpu() { return CPUPlace(); }
+const CUDAPinnedPlace default_cuda_pinned() { return CUDAPinnedPlace(); }
+
+bool is_gpu_place(const Place &p) {
+  return dynamic_cast<const CUDAPlace*>(&p) != nullptr;
+}
+
+bool is_cpu_place(const Place &p) {
+  return dynamic_cast<const CPUPlace*>(&p) != nullptr;
+}
+
+bool is_cuda_pinned_place(const Place &p) {
+  return dynamic_cast<const CUDAPinnedPlace*>(&p) != nullptr;
+}
+
+int which_place(const Place& p) {
+  int i = -1;
+  if (is_cpu_place(p))
+    i = 0;
+  else if (is_gpu_place(p))
+    i = 1;
+  else if (is_cuda_pinned_place(p))
+    i = 2;
+  return i;
+}
+
+bool places_are_same_class(const Place &p1, const Place &p2) {
+  return which_place(p1) == which_place(p2);
+}
+
+bool is_same_place(const Place &p1, const Place &p2) {
+  if (places_are_same_class(p1, p2)) {
+   return is_cuda_pinned_place(p1)
+       ? dynamic_cast<const CUDAPlace&>(p1).device
+       == dynamic_cast<const CUDAPlace&>(p2).device
+       : true;
+  }
+  return false;
+}
+
+std::ostream &operator<<(std::ostream & os, const Place & p) {
+  if (is_cpu_place(p))
+    os << "CPUPlace";
+  else if (is_gpu_place(p))
+    os << "CUDAPlace(" << dynamic_cast<const CUDAPlace&>(p).device << ")";
+  else if (is_cuda_pinned_place(p))
+    os << "CUDAPinnedPlace";
+  return os;
+}
+
+}  // namespace platform
+}  // namespace paddle
---
+++ paddle/fluid/platform/place.h
@@ -0,0 +1,90 @@
+/* Copyright (c) 2016 PaddlePaddle Authors. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License. */
+#pragma once
+
+#include <iostream>
+#include <vector>
+
+#include "paddle/fluid/platform/enforce.h"
+
+namespace paddle {
+namespace platform {
+
+struct Place {
+  virtual ~Place() {}
+};
+  
+struct CPUPlace : public Place {
+  CPUPlace() {}
+
+  // needed for variant equality comparison
+  inline bool operator==(const CPUPlace &) const { return true; }
+  inline bool operator!=(const CPUPlace &) const { return false; }
+};
+
+struct CUDAPlace : public Place {
+  CUDAPlace() : CUDAPlace(0) {}
+  explicit CUDAPlace(int d) : device(d) {}
+
+  inline int GetDeviceId() const { return device; }
+  // needed for variant equality comparison
+  inline bool operator==(const CUDAPlace &o) const {
+    return device == o.device;
+  }
+  inline bool operator!=(const CUDAPlace &o) const { return !(*this == o); }
+
+  int device;
+};
+
+struct CUDAPinnedPlace : public Place {
+  CUDAPinnedPlace() {}
+
+  // needed for variant equality comparison
+  inline bool operator==(const CUDAPinnedPlace &) const { return true; }
+  inline bool operator!=(const CUDAPinnedPlace &) const { return false; }
+};
+
+using PlaceList = std::vector<Place>;
+
+void set_place(const Place &);
+const Place &get_place();
+
+const CUDAPlace default_gpu();
+const CPUPlace default_cpu();
+const CUDAPinnedPlace default_cuda_pinned();
+
+bool is_gpu_place(const Place &);
+bool is_cpu_place(const Place &);
+bool is_cuda_pinned_place(const Place &);
+bool places_are_same_class(const Place &, const Place &);
+bool is_same_place(const Place &, const Place &);
+
+int which_place(const Place& p);
+  
+struct PlaceHash {
+  std::size_t operator()(const Place &p) const {
+    constexpr size_t num_dev_bits = 4;
+    std::hash<int> ihash;
+    size_t dev_id = 0;
+    if (is_gpu_place(p)) {
+      dev_id = dynamic_cast<const CUDAPlace&>(p).device;
+    }
+    return ihash(dev_id << num_dev_bits | which_place(p));
+  }
+};
+
+std::ostream &operator<<(std::ostream &, const Place &);
+
+}  // namespace platform
+}  // namespace paddle
